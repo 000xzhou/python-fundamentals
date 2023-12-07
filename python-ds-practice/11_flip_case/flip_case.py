8e76ef8fbee63cd50eda1843799438f1aa55506d
000xzhou <xzhou92@gmail.com>
2023-12-06 21:12:25 -0500

    flip case input

--- python-ds-practice/11_flip_case/flip_case.py
+++ python-ds-practice/11_flip_case/flip_case.py
@@ -11,3 +11,15 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    # get only the letter that's swaping
+    # ???????????
+    # swap
+    x = list(phrase)
+    for i, letter in enumerate(x):
+        if letter == to_swap or letter == to_swap.swapcase():
+            x[i] = letter.swapcase()
+    return "".join(x)
+    
+print(flip_case('Aaaahhh', 'a'))
+print(flip_case('Aaaahhh', 'A'))
+print(flip_case('Aaaahhh', 'h'))
