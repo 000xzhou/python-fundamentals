fe2f1b9b86950c2677bf43e2d720603050988f76
000xzhou <xzhou92@gmail.com>
2023-12-11 16:44:51 -0500

    36-38

--- python-ds-practice/36_find_the_duplicate/find_the_duplicate.py
+++ python-ds-practice/36_find_the_duplicate/find_the_duplicate.py
@@ -13,3 +13,16 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+    non_dup = set()
+    for num in nums:
+        if num not in non_dup:
+            non_dup.add(num)
+        else:
+            return num
+    return None
+        
+assert find_the_duplicate([1, 2, 1, 4, 3, 12]) == 1
+
+assert find_the_duplicate([6, 1, 9, 5, 3, 4, 9]) == 9
+
+assert find_the_duplicate([2, 1, 3, 4]) is None
--- python-ds-practice/37_sum_up_diagonals/sum_up_diagonals.py
+++ python-ds-practice/37_sum_up_diagonals/sum_up_diagonals.py
@@ -18,3 +18,11 @@ def sum_up_diagonals(matrix):
         >>> sum_up_diagonals(m2)
         30
     """
+    # (0,0)(1,1)(2,2)
+    # (0,2)(1,1)(2,0)
+    sum_x = 0
+    sum_y = 0
+    for i in range(len(matrix)):
+        sum_x += matrix[i][i]
+        sum_y += matrix[i][len(matrix)-1-i]
+    return sum_x + sum_y
